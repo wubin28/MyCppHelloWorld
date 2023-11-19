820864fe953d0f52fa37317017bdb43f54917013
Sun <wubin28@gmail.com>
2023-11-19 10:53:59 +0800

    fix: fix the errors with the help of chatgpt

--- MyCppHelloWorld.cpp
+++ MyCppHelloWorld.cpp
@@ -1,25 +1,38 @@
 #include<stdio.h>
-int n, num = 0;
+
 int main() {
-	printf("please enter an integer between 1 and 99999");
-	scanf_s("%d", &n);
-	if ((n <= 0) && (n > 9999))
-		printf("invalid input. please enter an integer between 1 and 99999.");
-	while (n) {
-		n /= 10;
-		num++;
-	}
-	printf("number of digits is %d\n", num);
-	printf("the digits of the integer from high to low are\n");
-	char n;
-	while ((n >= '0') && (n <= '9')) {
-		if (!(n >= '0') && (n <= '9'))
-			break;
-	}
-	printf("%c", n);
-	while (n != 0) {
-		printf("%d", n % 10);
-		n /= 10;
-	}
-	return 0;
+    int n, num = 0;
+    printf("Please enter an integer between 1 and 99999: ");
+    scanf_s("%d", &n);
+
+    if (n <= 0 || n > 99999) {
+        printf("Invalid input. Please enter an integer between 1 and 99999.\n");
+        return 1; // Exiting the program if the input is not valid
+    }
+
+    int temp = n; // Temporary variable to store the original number
+
+    // Counting the number of digits
+    while (temp) {
+        temp /= 10;
+        num++;
+    }
+
+    printf("Number of digits is %d\n", num);
+
+    printf("The digits of the integer from high to low are:\n");
+
+    int divisor = 1;
+    for (int i = 1; i < num; i++) {
+        divisor *= 10;
+    }
+
+    while (divisor) {
+        printf("%d ", n / divisor);
+        n %= divisor;
+        divisor /= 10;
+    }
+
+    printf("\n");
+    return 0;
 }
